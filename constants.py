b34ecdd693d44629866afe23a96549854d0b3e70
lexa <leo1563@rambler.ru>
2022-09-15 12:36:12 +0300

    add some other assets

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 # add other assets to this dict!
 assets_dict = {"USDT":"34N9YcEETLWn93qYQ64EsP1x89tSruJU44RrEMSXXEPJ",
-			   "USDN":"DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p"}
+			   "USDN":"DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p",
+			   "BUSD":"8DLiYZjo3UUaRBTHU7Ayoqg4ihwb6YH1AfXrrhdjQ7K1",
+			   "DAI":"8zUYbdB8Q6mDhpcXYv52ji8ycfj4SDX4gJXS7YY3dA4R"}
